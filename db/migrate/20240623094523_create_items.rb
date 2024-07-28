8bcc5f85bf4a0da2b6344b9fd880b8a79799a72e
mochimochi <hybridkazuhiko@yahoo.co.jp>
2024-07-28 18:06:01 +0900

    updated create items.rb

--- db/migrate/20240623094523_create_items.rb
+++ db/migrate/20240623094523_create_items.rb
@@ -1,6 +1,6 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
-    create_table :items do |t|
+    create_table :item do |t|
       t.string :thing
       t.string :place
       t.text   :image
